ef57a731c8a6520239d6ce55af5bfc4eb8ca4b04
Will Edwards <william.edwards@cix.csi.cuny.edu>
2022-03-31 12:31:09 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
               <Intro/>
               <AboutMe/>
               <Portfolio/>
-              <Works/>
+              {/* <Works/> */}
               <Contact/>
 
           </div>
